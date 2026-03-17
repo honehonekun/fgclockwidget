e3ccc7a4a12df3b448dd7377753c8d410d115084
honehonekun <117441174+honehonekun@users.noreply.github.com>
2026-03-17 22:29:51 +0900

    UI improvement

--- app/src/main/java/com/example/fgclockwidget/MainActivity.kt
+++ app/src/main/java/com/example/fgclockwidget/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,11 +16,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,7 +38,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
@@ -43,12 +55,20 @@ import com.github.skydoves.colorpicker.compose.rememberColorPickerController
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FgclockwidgetTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+                    CenterAlignedTopAppBar({
+                        Image(
+                            painterResource(R.drawable.ascii_art_text_removebg_preview),
+                            contentDescription = "Figlet Clock"
+                        )
+                    }, colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Black))
+                }) { innerPadding ->
                     Greeting(
                         modifier = Modifier
                             .fillMaxSize()
@@ -119,8 +139,37 @@ fun Greeting(context: Context, modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
     ) {
+
+        Card (border = BorderStroke(2.dp,color)){
+            Column (modifier= Modifier.padding(16.dp)){
+                HsvColorPicker(
+                    modifier = Modifier.size(256.dp),
+                    controller = controller,
+                    onColorChanged = { value ->
+                        color = value.color
+                    }
+                )
+                Spacer(Modifier.height(16.dp))
+                BrightnessSlider(
+                    //明暗スライダー
+                    modifier = Modifier
+                        .width(256.dp)
+                        .height(16.dp),
+                    controller = controller,
+                )
+            }
+        }
+        Spacer(Modifier.height(16.dp))
+        Button(onClick = {
+            scope.launch {
+                setColor(context, color)
+            }
+        }, shape = RoundedCornerShape(4.dp),modifier = Modifier.width(256.dp)
+        ) {
+            Text(text = "Set Color")
+        }
         Box(modifier = Modifier.padding(16.dp)) {
-            Button(onClick = { expanded = true }) {
+            Button(onClick = { expanded = true },shape =  RoundedCornerShape(4.dp),modifier = Modifier.width(256.dp)) {
                 Text("Choose Font")
             }
 
@@ -141,31 +190,6 @@ fun Greeting(context: Context, modifier: Modifier = Modifier) {
                     )
                 }
             }
-        }
-        HsvColorPicker(
-            modifier = Modifier.size(156.dp),
-            controller = controller,
-            onColorChanged = { value ->
-                color = value.color
-            }
-        )
-        Spacer(Modifier.height(16.dp))
-        BrightnessSlider(
-            //明暗スライダー
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(16.dp)
-                .padding(horizontal = 16.dp),
-            controller = controller,
-        )
-        Spacer(Modifier.height(16.dp))
-        Button(onClick = {
-            scope.launch {
-                setColor(context, color)
-            }
-        }
-        ) {
-            Text(text = "Set Color")
         }
 
 
